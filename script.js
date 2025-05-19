9e132110aa7186941757f76e3a7ca8264cdb1c1c
GNWekesa <gabriellanekwek@gmail.com>
2025-05-20 01:19:54 +0300

    removed JavaScript for rotating hobbies  because am using bootstrap class="carousel-item" to rotate items

--- script.js
+++ script.js
@@ -15,29 +15,7 @@ window.onscroll = function () {
     window.scrollTo({ top: 0, behavior: "smooth" });
   });
   
- /////////////////////////////////////////
-  //  JavaScript for rotating hobbies 
-  document.addEventListener("DOMContentLoaded", function () {
-    const hobbies = [
-      { icon: "bi-mic-fill", text: "Singing" },
-      { icon: "bi-headphones", text: "Listening to Music" },
-      { icon: "bi-geo-alt-fill", text: "Travelling" },
-      { icon: "bi-book-fill", text: "Reading" },
-      { icon: "bi-egg-fried", text: "Cooking" },
-      { icon: "bi-scissors", text: "Crocheting" },
-      { icon: "bi-tree-fill", text: "Hiking" },
-      { icon: "bi-droplet-fill", text: "Soap Making" }
-    ];
-
-    const hobbyElement = document.getElementById("hobbyText");
-    let index = 0;
-
-    setInterval(() => {
-      index = (index + 1) % hobbies.length;
-      const { icon, text } = hobbies[index];
-      hobbyElement.innerHTML = `<i class="bi ${icon} me-2"></i>${text}`;
-    }, 2500);
-  });
+ 
 
  /////////////////////////////////////////
   //script below is for loading greetings
